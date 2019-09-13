fbafa3e8a7e79a237dc51c1f5e22bc93755833b1
sturmenta <sturmenta@gmail.com>
2019-09-13 19:31:23 -0300

    slide-show

--- client.js
+++ client.js
@@ -1,23 +1,21 @@
-// This file contains the boilerplate to execute your React app.
-// If you want to modify your application's content, start in "index.js"
-
 import {ReactInstance} from 'react-360-web';
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
-    // Add custom options here
     fullScreen: true,
     ...options,
   });
 
-  // Render your app content to the default cylinder surface
   r360.renderToSurface(
-    r360.createRoot('react360_samples', { /* initial props */ }),
-    r360.getDefaultSurface()
+    r360.createRoot('SlideshowSample', {
+      photos: [
+        {uri: './static_assets/360_world.jpg', title: '360 World', format: '2D'},
+        // Add your own 180 / 360 photos to this array,
+        // with an associated title and format
+      ],
+    }),
+    r360.getDefaultSurface(),
   );
-
-  // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
 }
 
 window.React360 = {init};
