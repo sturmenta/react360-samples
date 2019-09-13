e0c3ca92696347110c63dd18265eafc2fa5a18fb
sturmenta <sturmenta@gmail.com>
2019-09-13 19:36:17 -0300

    headlocked-surfaces

--- index.js
+++ index.js
@@ -1,43 +1,32 @@
-import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-360';
+import * as React from 'react';
+import {AppRegistry, StyleSheet, Text, View} from 'react-360';
 
-export default class react360_samples extends React.Component {
-  render() {
-    return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
-          </Text>
-        </View>
-      </View>
-    );
-  }
-};
+const HorizontalPanel = () => (
+  <View style={styles.panel}>
+    <Text style={styles.panelText}>{'Follows Horizontally'}</Text>
+  </View>
+);
+
+const HVPanel = () => (
+  <View style={styles.panel}>
+    <Text style={styles.panelText}>{'Follows Horizontally\nand Vertically'}</Text>
+  </View>
+);
 
 const styles = StyleSheet.create({
   panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    width: 300,
+    height: 300,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  greetingBox: {
-    padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
-  },
-  greeting: {
+  panelText: {
+    color: '#000000',
     fontSize: 30,
-  },
+    textAlign: 'center',
+  }
 });
 
-AppRegistry.registerComponent('react360_samples', () => react360_samples);
+AppRegistry.registerComponent('HorizontalPanel', () => HorizontalPanel);
+AppRegistry.registerComponent('HVPanel', () => HVPanel);
