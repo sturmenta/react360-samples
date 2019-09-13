1897dd8746e524bbd5323fd997bc3a8dd56dfd00
sturmenta <sturmenta@gmail.com>
2019-09-13 19:35:31 -0300

    custom-player-sample

--- client.js
+++ client.js
@@ -2,22 +2,21 @@
 // If you want to modify your application's content, start in "index.js"
 
 import {ReactInstance} from 'react-360-web';
+import DashVideoPlayer from './DashVideoPlayer';
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
-    // Add custom options here
     fullScreen: true,
+    customVideoPlayers: [DashVideoPlayer],
     ...options,
   });
 
-  // Render your app content to the default cylinder surface
   r360.renderToSurface(
-    r360.createRoot('react360_samples', { /* initial props */ }),
+    r360.createRoot('CustomPlayerSample', { /* initial props */ }),
     r360.getDefaultSurface()
   );
 
-  // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+  r360.compositor.setBackground(r360.getAssetURL('chess-world.jpg'));
 }
 
 window.React360 = {init};
--- index.js
+++ index.js
@@ -1,43 +1,39 @@
 import React from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
   View,
+  StyleSheet,
+  asset,
 } from 'react-360';
+import VideoModule from 'VideoModule';
+import * as Environment from 'Environment';
 
-export default class react360_samples extends React.Component {
-  render() {
-    return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
-          </Text>
-        </View>
-      </View>
-    );
+const VIDEO_PLAYER = 'dash_video';
+const VIDEO_SOURCE =[
+  {
+    url: asset('video_dash_mp4/video_stream.mpd').uri, 
+    fileFormat: 'mp4',
+  },
+  {
+    url: asset('video_dash_webm/video_stream.mpd').uri, 
+    fileFormat: 'webm',
   }
-};
+];
 
-const styles = StyleSheet.create({
-  panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  greetingBox: {
-    padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
-  },
-  greeting: {
-    fontSize: 30,
-  },
-});
+class CustomPlayerSample extends React.Component {
+  componentDidMount() {
+    VideoModule.createPlayer(VIDEO_PLAYER);
+    VideoModule.play(VIDEO_PLAYER, {
+      source: VIDEO_SOURCE,
+      stereo: '2D',
+    });
 
-AppRegistry.registerComponent('react360_samples', () => react360_samples);
+    Environment.setScreen('default', VIDEO_PLAYER, 'default', 0, 0, 1000, 600);
+  }
+
+  render() {
+    return null;
+  }
+}
+
+AppRegistry.registerComponent('CustomPlayerSample', () => CustomPlayerSample);
